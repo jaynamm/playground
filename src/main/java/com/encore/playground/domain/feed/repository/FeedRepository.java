03b382fcb6437021f4a3bb6cc685ddaf0e596626
Jaynam <48994100+jaynamm@users.noreply.github.com>
2023-04-22 23:16:51 +0900

    Revert "4월 21일 피드 화면 백엔드 조율 결과"

--- src/main/java/com/encore/playground/domain/feed/repository/FeedRepository.java
+++
@@ -1,9 +0,0 @@
-package com.encore.playground.domain.feed.repository;
-
-import com.encore.playground.domain.feed.entity.Feed;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * 피드 글을 저장하는 리포지토리
- */
-public interface FeedRepository extends JpaRepository<Feed, Integer> {}
