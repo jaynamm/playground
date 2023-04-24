c59aabc294b30c6ba023fa3662910c631b466c45
moneyless1989 <moneyless1989@gmail.com>
2023-04-24 17:50:13 +0900

    Fix: 피드 화면 POC를 위해 로그인 관련 기능 삭제

--- src/main/java/com/encore/playground/domain/feed/control/FeedController.java
+++ src/main/java/com/encore/playground/domain/feed/control/FeedController.java
@@ -19,6 +19,13 @@ public class FeedController {
 
     private final FeedService feedService;
 
+    // 피드 메인페이지
+    @GetMapping(value = {"", "/"})
+    public String feedMain(Model model) {
+        model.addAttribute("feeds", feedService.feedPage());
+        return "feed/feed_main";
+    }
+    // 게시글 작성
     @PostMapping(value = "/write")
     public String write(String id, String article, Model model) {
         List<FeedDto> feedAfterWrite = feedService.write(id, article);
@@ -26,6 +33,7 @@ public class FeedController {
         return "feed/feed_main";
     }
 
+    // 게시글 수정 페이지로 이동
     @GetMapping(value = "/modify")
     public String modify(int feedNo, Model model) {
         FeedDto feedToModify = feedService.getFeed(feedNo);
@@ -33,6 +41,7 @@ public class FeedController {
         return "feed/feed_modify";
     }
 
+    // 게시글 수정
     @PostMapping(value = "/modify")
     public String modify(int feedNo, String article, Model model) {
         List<FeedDto> feedAfterModify = feedService.modify(feedNo, article);
@@ -40,16 +49,11 @@ public class FeedController {
         return "feed/feed_main";
     }
 
+    // 게시글 삭제
     @GetMapping(value = "/delete")
     public String delete(int feedNo, Model model) {
         List<FeedDto> feedAfterDel = feedService.delete(feedNo);
         model.addAttribute("feeds", feedAfterDel);
-        return "feed/feed_main";
-    }
-
-    @GetMapping(value = {"", "/"})
-    public String feedMain(Model model) {
-        model.addAttribute("feeds", feedService.feedPage());
         return "feed/feed_main";
     }
 }
