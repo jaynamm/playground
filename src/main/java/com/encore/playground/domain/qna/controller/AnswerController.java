52184424b98d42dab31829756b8fb7f511ff2f07
jaynamm <polky0421@gmail.com>
2023-05-18 17:24:03 +0900

    Refactor: 답변 컨트롤러 기능 추가

--- src/main/java/com/encore/playground/domain/qna/controller/AnswerController.java
+++ src/main/java/com/encore/playground/domain/qna/controller/AnswerController.java
@@ -1,12 +1,12 @@
 package com.encore.playground.domain.qna.controller;
 
-import com.encore.playground.domain.qna.dto.AnswerDTO;
+import com.encore.playground.domain.qna.dto.AnswerDto;
+import com.encore.playground.domain.qna.dto.QuestionDto;
 import com.encore.playground.domain.qna.service.AnswerService;
-import lombok.Getter;
+import com.encore.playground.domain.qna.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,9 +15,28 @@ import java.util.Map;
 @RestController
 public class AnswerController {
     private final AnswerService answerService;
+    private final QuestionService questionService;
 
-    @PostMapping("/answer/create")
-    private List<AnswerDTO> createAnswer(@RequestBody AnswerDTO answerDTO) {
-        return answerService.create(answerDTO);
+    @PostMapping("/answer/list")
+    private List<AnswerDto> answerList(@RequestBody Map<String, Long> getQuestionId) {
+        Long questionId = getQuestionId.get("question_id");
+        return answerService.answerList(questionId);
+    }
+
+    /**
+     * POST - 질문 답변 생성
+     * @param id
+     * @param answerDTO
+     * @return List<AnswerDTO>
+     */
+    @PostMapping("/answer/create/{id}")
+    private List<AnswerDto> createAnswer(@PathVariable Long id, @RequestBody AnswerDto answerDTO) {
+        System.out.println("Question ID = " + id);
+
+        QuestionDto questionDto = questionService.readQuestion(id);
+
+        System.out.println(questionDto);
+
+        return answerService.create(answerDTO, questionDto);
     }
 }
