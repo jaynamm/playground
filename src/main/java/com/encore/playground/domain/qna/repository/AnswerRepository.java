5ce37678826ccd6c86aeb603a9bce14017197d4c
jaynamm <polky0421@gmail.com>
2023-05-18 17:19:53 +0900

    Feat: 아이디 패스워드 찾기를 위한 DTO 생성

--- src/main/java/com/encore/playground/domain/qna/repository/AnswerRepository.java
+++ src/main/java/com/encore/playground/domain/qna/repository/AnswerRepository.java
@@ -4,6 +4,8 @@ import com.encore.playground.domain.qna.entity.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 }
