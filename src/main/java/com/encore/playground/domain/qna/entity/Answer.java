4f75f206a9e734e747af9e7f39f10863ae7037bd
jaynamm <polky0421@gmail.com>
2023-05-18 17:17:49 +0900

    Refactor: 엔티티 컬럼 변경

--- src/main/java/com/encore/playground/domain/qna/entity/Answer.java
+++ src/main/java/com/encore/playground/domain/qna/entity/Answer.java
@@ -1,5 +1,6 @@
 package com.encore.playground.domain.qna.entity;
 
+import com.encore.playground.domain.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
@@ -15,17 +16,20 @@ import java.time.LocalDateTime;
 @Table(name="answer")
 public class Answer {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
+    // TODO: author 를 member_id 로 참조해서 가져와야한다.
     @Column(nullable = false)
-    private String author;
+    private String memberId;
 
     @CreatedDate
     private LocalDateTime createdDate;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "question_id")
     private Question question;
 }
