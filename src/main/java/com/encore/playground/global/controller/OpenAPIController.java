0d4f94d46e7125b7d18795acc95463f9f5da5b92
moneyless1989 <moneyless1989@gmail.com>
2023-06-23 18:53:25 +0900

    Feat: swagger 설정 시작

---
+++ src/main/java/com/encore/playground/global/controller/OpenAPIController.java
@@ -0,0 +1,11 @@
+package com.encore.playground.global.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/apiui/temp")
+@Tag(name = "Playground", description = "플레이그라운드 API 문서")
+public class OpenAPIController {
+}
---
+++ src/main/java/com/encore/playground/global/swagger/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.encore.playground.global.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Playground API")
+                .description("Playground API 명세서");
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}
