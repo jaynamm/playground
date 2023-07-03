ff5b5ee7cf03839e725c2a844fc1069323ddf3ee
moneyless1989 <moneyless1989@gmail.com>
2023-07-03 19:01:34 +0900

    Doc: 테스트용 sql파일 변경

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,14 @@
-insert into feed (comment_count, comment_total_count, content, created_date, like_count, member_id, modified_date, view_count) values (0, 0, "나 오늘 버거킹 불고기 와퍼 먹을 예정", '2023-05-18T19:27:41.487261', 0, "유수빈", '2023-05-18T19:27:41.487261', 0);
-insert into feed (comment_count, comment_total_count, content, created_date, like_count, member_id, modified_date, view_count) values (0, 0, "아 배고파 그래서 오점뭐?", '2023-05-18T19:27:41.487261', 0, "남민우", '2023-05-18T19:27:41.487261', 0);
-insert into feed (comment_count, comment_total_count, content, created_date, like_count, member_id, modified_date, view_count) values (0, 0, "오늘 맥주 먹을 사람? 고량주 ㄱ?", '2023-05-18T19:27:41.487261', 0, "김용호", '2023-05-18T19:27:41.487261', 0);
-insert into feed (comment_count, comment_total_count, content, created_date, like_count, member_id, modified_date, view_count) values (0, 0, "이클립스나 멘토스 혹시 더 있나요?", '2023-05-18T19:27:41.487261', 0, "권광일", '2023-05-18T19:27:41.487261', 0);
+INSERT INTO playground.member (id, created_date, curriculum, email, name, nickname, password, modified_date, role, userid) VALUES (1, '2023-05-17 16:54:04.593788', 'qwer', 'suub13@qwer', '유수빈', 'Amelia', '$2a$10$9KexgHrV.lFGxIonm8Q7yuwvEfwMeoi2YSo.uYznryT1Kypoa/tMG', '2023-05-17 16:54:04.593788', 'USER', 'suub13');
+INSERT INTO playground.member (id, created_date, curriculum, email, name, nickname, password, modified_date, role, userid) VALUES (2, '2023-05-17 16:54:04.593788', 'qwer', 'mandoo1229@qwer', '남민우', 'Donald', '$2a$10$9KexgHrV.lFGxIonm8Q7yuwvEfwMeoi2YSo.uYznryT1Kypoa/tMG', '2023-05-17 16:54:04.593788', 'USER', 'mandoo1229');
+INSERT INTO playground.member (id, created_date, curriculum, email, name, nickname, password, modified_date, role, userid) VALUES (3, '2023-05-17 16:54:04.593788', 'qwer', 'KKYHH@qwer', '김용호', 'Nicolas', '$2a$10$9KexgHrV.lFGxIonm8Q7yuwvEfwMeoi2YSo.uYznryT1Kypoa/tMG', '2023-05-17 16:54:04.593788', 'USER', 'KKYHH');
+INSERT INTO playground.member (id, created_date, curriculum, email, name, nickname, password, modified_date, role, userid) VALUES (4, '2023-05-17 16:54:04.593788', 'qwer', 'moneyless1989@qwer', '권광일', 'Richard', '$2a$10$9KexgHrV.lFGxIonm8Q7yuwvEfwMeoi2YSo.uYznryT1Kypoa/tMG', '2023-05-17 16:54:04.593788', 'USER', 'moneyless1989');
+INSERT INTO playground.member (id, created_date, curriculum, email, name, nickname, password, modified_date, role, userid) VALUES (5, '2023-05-17 16:54:04.593788', 'qwer', 'qwer@qwer', '테스트유저', 'qwer', '$2a$10$9KexgHrV.lFGxIonm8Q7yuwvEfwMeoi2YSo.uYznryT1Kypoa/tMG', '2023-05-17 16:54:04.593788', 'USER', 'qwer');
+INSERT INTO playground.member (id, created_date, curriculum, email, name, nickname, password, modified_date, role, userid) VALUES (6, '2023-05-17 16:54:04.593788', 'qwer', 'admin@qwer', '테스트운영자', 'admin', '$2a$10$9KexgHrV.lFGxIonm8Q7yuwvEfwMeoi2YSo.uYznryT1Kypoa/tMG', '2023-05-17 16:54:04.593788', 'ADMIN', 'admin');
 
-insert into member(userid, email, name, nickname, password, created_date, modified_date, role) values ("adminaccount", "admin@gmail.com", "admin", "manager", "$2a$10$fXHD8jxO.eTBvduwZlTzx.J3fAEPUjlSx5eUOT4JYUS7700dFYY.K", 20230518091726, 20230518091726, "ADMIN");
-insert into member(userid, email, name, nickname, password, created_date, modified_date, role) values ("useraccount", "user@gmail.com", "user", "student", "$2a$10$fXHD8jxO.eTBvduwZlTzx.J3fAEPUjlSx5eUOT4JYUS7700dFYY.K", 20230518091726, 20230518091726, "USER");
+insert into feed (content, created_date, like_count, member_id, modified_date, view_count) values ("나 오늘 버거킹 불고기 와퍼 먹을 예정", '2023-05-18T19:27:41.487261', 0, 1, '2023-05-18T19:27:41.487261', 0);
+insert into feed (content, created_date, like_count, member_id, modified_date, view_count) values ("아 배고파 그래서 오점뭐?", '2023-05-18T19:27:41.487261', 0, 2, '2023-05-18T19:27:41.487261', 0);
+insert into feed (content, created_date, like_count, member_id, modified_date, view_count) values ("오늘 맥주 먹을 사람? 고량주 ㄱ?", '2023-05-18T19:27:41.487261', 0, 3, '2023-05-18T19:27:41.487261', 0);
+insert into feed (content, created_date, like_count, member_id, modified_date, view_count) values ("이클립스나 멘토스 혹시 더 있나요?", '2023-05-18T19:27:41.487261', 0, 4, '2023-05-18T19:27:41.487261', 0);
 
 insert into notice(title, content, created_date, modified_date, member_id) values ("[안내] 캠퍼스 이전 관련 공지입니다.", "현재 운영 중인 독산 캠퍼스를 수원으로 이전하게 되었습니다.", 20230512120122, 20230512120122, 1);
 insert into notice(title, content, created_date, modified_date, member_id) values ("[긴급] 교재 수령 안내", "21기 수강생 여러분들은 빠른 시일 내에 교재를 받지 않으면 큰일이 납니다.", 20230516200134, 20230516200134, 1);
@@ -13,5 +17,5 @@ insert into notice(title, content, created_date, modified_date, member_id) value
 insert into notice(title, content, created_date, modified_date, member_id) values ("[신청]  LET'S PLAY 현직자 Session 사전조사 ", "의견을 반영하고자 사전 설문조사를 진행하고자 하니 사전조사 설문에 참여해주십시오.", 20230518224202, 20230518224202, 1);
 
 insert into question(title, content, member_id, created_date) values ("공가 사용에 대해 질문이 있습니다.", "한번에 공가를 몇개까지 사용할 수 있나요? ", 2, 20230518091726);
-insert into question(title, content, member_id, created_date) values ("독산 캠퍼스 운영시간이 궁금합니다.", "몇시까지 운영하나요? ", 2, 20230519221726);
-insert into question(title, content, member_id, created_date) values ("도서 대출 관련 질문입니다.", "책 대여기간은 며칠인가요?", 2, 20230519231726);
+insert into question(title, content, member_id, created_date) values ("독산 캠퍼스 운영시간이 궁금합니다.", "몇시까지 운영하나요? ", 3, 20230519221726);
+insert into question(title, content, member_id, created_date) values ("도서 대출 관련 질문입니다.", "책 대여기간은 며칠인가요?", 1, 20230519231726);
